0b8c1cdb7474836f53d53a31bbcde20b4dd9fd9b
Richardson <104580727+codewithrich@users.noreply.github.com>
2023-09-02 11:33:57 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    
+    <footer>
+    
+     Made with ♡ by <a target='blank' href='https://github.com/codewithrich'>Richardson.D</a>  &nbsp;|  &copy;{new Date().getFullYear()}
+    
+    </footer>
+
+  )
+}
+
+export default Footer;
